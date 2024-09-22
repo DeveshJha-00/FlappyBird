02d687ada77a2a13caca1ce2f73ab707aedcfa5c
DeveshJha-00 <deveshsanjiv16@gmail.com>
2024-09-22 19:35:28 +0530

    final-changes

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -62,6 +62,9 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
     Timer gameLoop;
     Timer placingPipesLoop;
 
+    boolean gameOver = false;
+    double score = 0;
+
 
     FlappyBird(){
         setPreferredSize(new Dimension(width,height));
@@ -87,7 +90,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         });
 
         // game timer
-        gameLoop = new Timer(1000/60,this); // 60FPS--> we wanna draw 60 frames per sec 
+        gameLoop = new Timer(1000/60,this); // 60FPS--> we wanna draw 60 frames per sec -> implements keylistener
         gameLoop.start();
         placingPipesLoop.start();
         
@@ -102,15 +105,47 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
         // pos-height/4-0 TO pos-height/4 - height/2 -->
         // 1/4 height -> 3/4 height
         int randomPipeYpos = (int)(pipeYpos - pipeHeight/4 - Math.random()*(pipeHeight/2)); 
+        int passingSpace = pipeHeight/4;
+        
         Pipe topPipe = new Pipe(topPipeImg);
         topPipe.y = randomPipeYpos;
         pipes.add(topPipe);
+
+        Pipe bottomPipe = new Pipe(bottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + passingSpace ;
+        pipes.add(bottomPipe);
     }
+
+
+    public void move(){       
+        // bird
+        bird.y += speedY;  //bird moving up
+        speedY += gravity; //bird falling down
+        bird.y = Math.max(0,bird.y); //limit to top of screen
+        
+        //pipes 
+        for(int i=0;i<pipes.size();i++){
+            Pipe pipe = pipes.get(i);
+            pipe.x += speedX; //pipe moving left
+            if (collision(bird,pipe)) gameOver = true;
+            if (!pipe.pipePassed && bird.x > pipe.x+pipe.width){
+                pipe.pipePassed = true;
+                score += 0.5;
+            }
+        }
+
+        if (bird.y > height){
+            gameOver = true;
+        }
+
+    }
+
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         drawSomething(g);
     }
+
 
     public void drawSomething(Graphics g){
         //drawing background
@@ -125,40 +160,59 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
             g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
         }
 
-    }
-
-    // move() will update x and y positions of all objects inside
-    public void move(){
-        //bird
-        bird.y += speedY;  //bird moving up
-        speedY += gravity; //bird falling down
-        bird.y = Math.max(0,bird.y); //limit to top of screen
-        
-        //pipes
-        for(int i=0;i<pipes.size();i++){
-            Pipe pipe = pipes.get(i);
-            pipe.x += speedX;
+        //score
+        g.setColor(Color.white);
+        g.setFont(new Font("sans-serrif",Font.PLAIN,30));
+        if (gameOver){
+            g.drawString("GAME OVER!",9,35);
+            g.drawString("SCORE : "+ String.valueOf((int)score),9,63);
+        } else{
+            g.drawString(String.valueOf((int) score),10,35);
         }
     }
 
+
+    public boolean collision(Bird bird, Pipe pipe){
+        return  bird.x < pipe.x + pipe.width &&
+                bird.x + bird.width > pipe.x &&
+                bird.y < pipe.y + pipe.height &&
+                bird.y + bird.height > pipe.y;
+    }
+
+
+    
+
+    //ACTION-PERFORMED METHODS
     @Override
     public void actionPerformed(ActionEvent e) {
-
         move();
-
+        if (gameOver){
+            placingPipesLoop.stop();
+            gameLoop.stop();
+        }
         repaint(); //this will call paintComponent() and redraw all the components bcz paintComponent is calling drawSomething() which draws all components and this will render 60 frames per sec due to timer passed as arguement during Timer class' calling
-
     }
 
 
+    //KEYLISTENERS
     @Override
     public void keyPressed(KeyEvent e) {
-        //on any key
-        if (e.getKeyCode() == KeyEvent.VK_SPACE){
-            speedY = -7; //reset 
-        }
-    }
 
+        if (e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_UP){
+            speedY = -7; //reset 
+            if (gameOver){
+                //reset conditions
+                gameOver = false;
+                bird.y = birdYpos;
+                bird.x = birdXpos;
+                pipes.clear();
+                gameLoop.start();
+                placingPipesLoop.start();
+                
+            }
+        }
+
+    }
     @Override
     public void keyReleased(KeyEvent e) {}
     @Override
